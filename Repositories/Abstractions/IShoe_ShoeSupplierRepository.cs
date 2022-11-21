a7b49c529fa606254618ffebbccfc808f322d340
Filip Omarchevski <filipomarchvski@gmail.com>
2022-11-21 15:24:55 +0200

    Add repository for the shoe supplier and for the joined table

---
+++ Repositories/Abstractions/IShoe_ShoeSupplierRepository.cs
@@ -0,0 +1,17 @@
+﻿using CourseProject.Models;
+
+namespace CourseProject.Repositories.Abstractions
+{
+    public interface IShoe_ShoeSupplierRepository
+    {
+        Shoe_ShoeSupplier GetById(int id);
+
+        List<Shoe_ShoeSupplier> GetAll();
+
+        void Insert(Shoe_ShoeSupplier shoeSupplier);
+
+        void Update(Shoe_ShoeSupplier shoeSupplier);
+
+        void Delete(int id);
+    }
+}
---
+++ Repositories/Implementations/Shoe_ShoeSupplierRepository.cs
@@ -0,0 +1,60 @@
+﻿using CourseProject.Models;
+using CourseProject.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace CourseProject.Repositories.Implementations
+{
+    public class Shoe_ShoeSupplierRepository : IShoe_ShoeSupplierRepository
+    {
+        private readonly NoFakeShoesDbContext noFakeShoesDbContext;
+
+        private readonly DbSet<Shoe_ShoeSupplier> shoe_ShoeSupplierDbSet;
+
+        public Shoe_ShoeSupplierRepository(NoFakeShoesDbContext noFakeShoesDbContext)
+        {
+            this.noFakeShoesDbContext = noFakeShoesDbContext;
+            shoe_ShoeSupplierDbSet = noFakeShoesDbContext.Set<Shoe_ShoeSupplier>();
+        }
+
+        public Shoe_ShoeSupplier GetById(int id)
+        {
+            return noFakeShoesDbContext.Shoe_ShoeSuppliers
+                 .Include(shoe_ShoeSupplier => shoe_ShoeSupplier.ShoeSupplier)
+                 .Include(shoe_ShoeSupplier => shoe_ShoeSupplier.Shoe)
+                 .SingleOrDefault(shoe_ShoeSupplier => shoe_ShoeSupplier.Id == id);
+        }
+
+        public List<Shoe_ShoeSupplier> GetAll()
+        {
+            return noFakeShoesDbContext.Shoe_ShoeSuppliers
+                .Include(shoe_ShoeSupplier => shoe_ShoeSupplier.ShoeSupplier)
+                .Include(shoe_ShoeSupplier => shoe_ShoeSupplier.Shoe)
+                .ToList();
+        }
+
+        public void Insert(Shoe_ShoeSupplier shoe_shoeSupplier)
+        {
+            shoe_ShoeSupplierDbSet.Add(shoe_shoeSupplier);
+            noFakeShoesDbContext.SaveChanges();
+        }
+
+        public void Update(Shoe_ShoeSupplier shoe_shoeSupplier)
+        {
+            Shoe_ShoeSupplier shoe_shoeSupplierInDB = GetById(shoe_shoeSupplier.Id);
+
+            if (shoe_shoeSupplierInDB != null)
+            {
+                noFakeShoesDbContext.Entry(shoe_shoeSupplierInDB).State = EntityState.Detached;
+            }
+            noFakeShoesDbContext.Entry(shoe_shoeSupplierInDB).State = EntityState.Modified;
+            noFakeShoesDbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            shoe_ShoeSupplierDbSet.Remove(GetById(id));
+            noFakeShoesDbContext.SaveChanges();
+        }
+    }
+}
