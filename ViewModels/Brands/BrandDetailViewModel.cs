1092ed1b84b35d76c917b562018cd07795dc7e80
elena.kikiova <elena.kikiova@primeholding.com>
2022-12-05 16:12:36 +0200

    Copy brand files

---
+++ ViewModels/Brands/BrandDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CourseProject.ViewModels.Shoes;
+using System.ComponentModel;
+
+namespace CourseProject.ViewModels.Brands
+{
+    public class BrandDetailViewModel
+    {
+        [DisplayName("Brand ID: ")]
+        public int Id { get; set; }
+
+        [DisplayName("Brand name: ")]
+        public string BrandName { get; set; }
+
+        public List<ShoeDetailsViewModel> Shoes;
+    }
+}
---
+++ ViewModels/Brands/CreateEditBrandViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CourseProject.ViewModels.Shoes;
+using Microsoft.Build.Framework;
+
+namespace CourseProject.ViewModels.Brands
+{
+    public class CreateEditBrandViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string BrandName { get; set; }
+
+        public List<SelectableShoesViewModel> Shoes { get; set; }
+    }
+}
